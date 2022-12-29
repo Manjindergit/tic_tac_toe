ee1e1d86d91caf560bbadbab998e9235fdc8cdea
Manjindergit <manjindergrewal1997@gmail.com>
2022-12-28 22:07:03 -0800

    working UI

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
+
+const buttons = document.querySelectorAll('.box');
+document.querySelector('.play').addEventListener('click', playGame);
+const arr = new Array(9).fill(null);
+
+
+
 const playerFactory = (name, sign) => {
     //creating new players
     return { name, sign }
 }
+
+const player1 = playerFactory('Player1', 'X');
+const player2 = playerFactory('Player2', 'O');
+let sign=player1.sign;
 
 const displayRender = (arr) => {
     //display being updated
@@ -9,54 +20,56 @@ const displayRender = (arr) => {
 }
 
 const gameBoard = () => {
-    let arr = [[[], [], []], [[], [], []], [[], [], []]];
 
 
-    const player1 = playerFactory('Player1', 'X');
-    const player2 = playerFactory('Player2', 'O');
 
-
-    console.log(arr);
 
     return { arr };
 }
 
-function updateMarks() {
-    const buttons = document.querySelectorAll('.box');
-    buttons.forEach((div) => {
-
-        div.addEventListener('click', () => {
-            div.textContent = sign;
-        });
+function updateMarks(index) {
+    console.log('insuide update narks');
+  //  arr[this.className.split(" ").pop()]=sign;
+    this.textContent=sign;
+    checkWinner(sign);
 
 
+    
 
-    });
+
 }
 
-console.log(updateMarks());
-/*
-function deleteToggle(){
-    const buttons = document.querySelectorAll('button');
-    const checkbox=document.querySelectorAll('checkbox');
-    console.log(checkbox);
-    console.log(buttons);
+function playGame() {
+
     
-    // we use the .forEach method to iterate through each button
-    buttons.forEach((button) => {
-    
-    
-    
-      // and for each one we add a 'click' listener
-      if(output.contains(button)){
-      button.addEventListener('click', () => {
-        myLibrary[button.className.split(" ").pop()].setRead();
-        console.log("yes it come in event listner");
-    
-      });
-    
-    
+
+   
+}
+//console.log(updateMarks());
+function signSwap(a){
+    console.log('inside signswap');
+    if(a==='X'){
+        a='O';
     }
-    });
+
+    else
+    a='X';
+}
+
+function checkWinner(){
     
-    }*/
+    for (let index = 0; index < length; index++) {
+        console.log(arr);
+}
+signSwap();
+}
+
+buttons.forEach((div) => {
+
+    div.addEventListener('click',
+        updateMarks, signSwap,{ once: true });
+
+
+
+});
+
