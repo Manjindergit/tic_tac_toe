675a24ff0991a0560d86968c284b03b0c71840d7
Manjindergit <manjindergrewal1997@gmail.com>
2022-12-28 22:47:16 -0800

    checkwinner 1st testcase wrking

--- script.js
+++ script.js
@@ -5,14 +5,15 @@ const arr = new Array(9).fill(null);
 
 
 
-const playerFactory = (name, sign) => {
+const playerFactory = (name) => {
     //creating new players
-    return { name, sign }
+    return { name }
 }
 
-const player1 = playerFactory('Player1', 'X');
-const player2 = playerFactory('Player2', 'O');
-let sign=player1.sign;
+const player1 = playerFactory('Player1');
+const player2 = playerFactory('Player2');
+let sign = 'X';
+//console.log({player1.sign})
 
 const displayRender = (arr) => {
     //display being updated
@@ -27,55 +28,55 @@ const gameBoard = () => {
     return { arr };
 }
 
-function updateMarks(index) {
-    console.log('insuide update narks');
-  //  arr[this.className.split(" ").pop()]=sign;
-    this.textContent=sign;
-    if(sign==='X'){
-        sign='O';
+function updateMarks() {
+
+    //  arr[this.className.split(" ").pop()]=sign;
+    this.textContent = sign;
+    arr[this.className.split(" ").pop()] = sign;
+    checkWinner();
+
+    if (sign === 'X') {
+        sign = 'O';
     }
 
-    else{
-    sign='X';
+    else {
+        sign = 'X';
     }
 
 
-    
+
 
 
 }
 
 function playGame() {
 
-    
 
-   
+
+
 }
-//console.log(updateMarks());
-function signSwap(a){
-    console.log('inside signswap');
-    if(a==='X'){
-        a='O';
+
+
+function checkWinner() {
+
+
+    for (let j = 0; j < 3; j++) {
+
+
+        if (arr[j] === arr[j + 3] && arr[j + 3] === arr[j + 6] && arr[j] !== null) {
+            console.log('game over', arr[j], j);
+        }
+
+
+
     }
 
-    else{
-    a='X';
-    }
-    return a;
-}
-
-function checkWinner(){
-    
-    for (let index = 0; index < length; index++) {
-        console.log(arr);
-}
-signSwap();
 }
 
 buttons.forEach((div) => {
 
     div.addEventListener('click',
-        updateMarks, signSwap,console.log(sign),{ once: true });
+        updateMarks, { once: true });
 
 
 
